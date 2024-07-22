02a02d70c1dd1e4a14ecf9b04989a50d615dbfec
nzaumuimi <nzaumuimi@gmail.com>
2024-07-22 08:39:26 -0700

    0x03-log_parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 import random
 import sys
 from time import sleep
 import datetime
 
 for i in range(10000):
-        sleep(random.random())
-            sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-                        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-                                datetime.datetime.now(),
-                                        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-                                                random.randint(1, 1024)
-                                                    ))
-                sys.stdout.flush()
+    sleep(random.random())
+    sys.stdout.write(
+        "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /ojs/260 HTTP/1.1\" {} {}\n".format(
+            random.randint(1, 255), random.randint(1, 255),
+            random.randint(1, 255), random.randint(1, 255),
+            datetime.datetime.now().strftime("%d/%b/%Y:%H:%M:%S +0000"),
+            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+            random.randint(1, 1024)
+
+        )
+     )
+    sys.stdout.flush()
