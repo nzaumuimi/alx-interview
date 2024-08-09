2cf53b83b5c927e32b326f7c033a8b5048c7de12
nzaumuimi <nzaumuimi@gmail.com>
2024-08-09 08:40:02 -0700

    0x05-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -48,23 +48,23 @@ class NQueen:
                     return self.res
 
 
-# Main
+    # Main
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-N = sys.argv[1]
+    N = sys.argv[1]
 
-try:
-    N = int(N)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    try:
+        N = int(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
     
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
     queen = NQueen(N)
     res = queen.nQueen(1)
