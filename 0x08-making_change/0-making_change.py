022be226df1b972ed22365ec4c805f70c7d57070
nzaumuimi <nzaumuimi@gmail.com>
2024-08-22 17:55:22 -0700

    0x08-making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""A function to determine the fewest number of coins needed
-   to meet a given amount total"""
+"""Module for making change"""
 
 
 def makeChange(coins, total):
-   """This function will take a list of coins and use
-   that to calculate how much change the total will require
-   """
-   if total <= 0:
-       return 0
+    """Function for clasic Bottom-Up dynamic programming"""
+    tempo_value = 0
+    coins.sort(reverse=True)
 
-   else:
-       coin = sorted(coins)
-       coin.reverse()
-       counter = 0
-       for i in coin:
-           while(total >= i):
-               counter += 1
-               total -= i
-               if total == 0:
-                   return counter
-               return -1
+    if total < 0:
+        return 0
+
+    for coin in coins:
+        if total % coin <= total:
+            tempo_value += total // coin
+            total = total % coin
+
+    return tempo_value if total == 0 else -1
+
+
+if __name__ == '__main__':
+
+    print(makeChange([1, 2, 25], 37))
+
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
