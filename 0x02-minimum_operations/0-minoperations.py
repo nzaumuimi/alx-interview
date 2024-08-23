64407bad48f0f7847b066ed8a334ed0faa55980b
nzaumuimi <nzaumuimi@gmail.com>
2024-08-22 17:35:45 -0700

    alx-interview

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,23 +4,23 @@
 
 
 def minOperations(n):
-        """
-            In a text file, there is 1 char H. Your text editor can execute
-                only 2 oprs in this file: Copy All and Paste. Given a # n,
-                    write a method that calculates the slowest number of opr needed to
-                        result in exactly n H characters in the file.
-                            Returns an int
-                                If n is impossible to achieve, returns 0
-                                    """
-                                        if not isinstance(n, int):
-                                                    return 0
+    """
+    In a text file, there is 1 char H. Your text editor can execute
+    only 2 oprs in this file: Copy All and Paste. Given a # n,
+    write a method that calculates the slowest number of opr needed to
+    result in exactly n H characters in the file.
+    Returns an int
+    If n is impossible to achieve, returns 0
+    """
+    if not isinstance(n, int):
+        return 0
 
-                                                    operations = 0
-                                                        iterator = 2
-                                                            while (iterator <= n):
-                                                                        if not (n % iterator):
-                                                                                        n = int(n / iterator)
-                                                                                                    operations += iterator
-                                                                                                                iterator = 1
-                                                                                                                        iterator += 1
-                                                                                                                            return operations
+    operations = 0
+    iterator = 2
+    while (iterator <= n):
+        if not (n % iterator):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
+    return operations
